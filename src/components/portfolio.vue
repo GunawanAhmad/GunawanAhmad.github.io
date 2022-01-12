d7091af4773fbf00adb4f5fe08707735152667f3
Gunawan Nur Ahmad <Gunawanfsdev@gmail.com>
2022-01-13 01:04:45 +0700

    fixing outline text and change font

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -1,20 +1,10 @@
 <template>
     <div class="portfolio section">
-        <svg class="outline-text">
-            <text
-                text-anchor="middle"
-                font-size="250"
-                fill="none"
-                stroke-width=".4"
-                font-weight="700"
-                stroke="#fff"
-                font-family="Montserrat"
-            >
-                Portfolio
-            </text>
-        </svg>
         <div class="text">
-            <h1 class="title">My Portfolio</h1>
+            <h1 class="title">
+                My Portfolio
+                <div class="outline-text">Portfolio</div>
+            </h1>
             <p class="deks">A small gallery of my recent projects</p>
         </div>
         <div @click="menuBackgroundAnim(1)" ref="btn" class="btn">
